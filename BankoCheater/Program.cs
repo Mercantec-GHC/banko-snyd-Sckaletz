ceb151932098447620d391c89025395a96add27e
Sckaletz <allan@leeloo.dk>
2024-02-26 09:19:42 +0100

    Update

--- BankoCheater/Program.cs
+++ BankoCheater/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] numbers = { 19, 10, 10, 10, 10 };
+
+            Console.WriteLine(numbers);
         }
     }
 }
