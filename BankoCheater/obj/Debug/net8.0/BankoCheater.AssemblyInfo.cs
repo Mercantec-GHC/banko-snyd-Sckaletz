bde00ed0060f98b420e4d38cc00e977e890510a4
Sckaletz <allan@leeloo.dk>
2024-02-26 07:39:13 +0100

    Update

--- BankoCheater/obj/Debug/net8.0/BankoCheater.AssemblyInfo.cs
+++ BankoCheater/obj/Debug/net8.0/BankoCheater.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankoCheater")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59be87870dca31625a47e9262e7c7d4075284671")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankoCheater")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankoCheater")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
